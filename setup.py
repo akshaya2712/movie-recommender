55fe4a1fc8a823015acb69f8760d16f0f029159e
Gayatri Akshaya <98305752+akshaya2712@users.noreply.github.com>
2024-07-14 12:10:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 with open("README.md","r",encoding="utf-8") as fh:
-    long_description=fh.read()
+    long_description=f.read()
+    
 AUTHOR_NAME='GayatriAkshaya'
 SRC_REPO='src'
 LIST_REQUIREMENTS=['streamlit']
@@ -8,6 +9,7 @@ setup(
       name=SRC_REPO,
       version='0.0.1',
       author=AUTHOR_NAME,
+      url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
       author_email='akshayavobbilisetti@gmail.com',
       long_description=long_description,
       long_description_content_type='text/markdown',
